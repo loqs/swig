7fc67570be2156544763bf364784f53b6b3c1878
William S Fulton <wsf@fultondesigns.co.uk>
2022-01-20 22:24:53 +0000

    Few more PCRE to PCRE2 changes
    (cherry picked from commit f8e4a5cc250ad8ee885061e30de8084f7d57981b)
    
     Conflicts:
    	Doc/Manual/SWIG.html
    	Doc/Manual/Windows.html
    	Source/Modules/main.cxx

--- Source/Swig/misc.c
+++ Source/Swig/misc.c
@@ -1449,7 +1449,7 @@ String *Swig_pcre_version(void) {
   char *buf = malloc(len);
   String *result;
   pcre2_config(PCRE2_CONFIG_VERSION, buf);
-  result = NewStringf("PCRE Version: %s", buf);
+  result = NewStringf("PCRE2 Version: %s", buf);
   free(buf);
   return result;
 }
